194b4eadff26f12fd9c9ddfffe81aa0e8cb8f73c
Shukrullo <shukrulloqurbonov342@gmail.com>
2025-05-04 22:24:49 +0500

    fixed issues

--- frontend/src/components/Layout/LayoutContent.jsx
+++ frontend/src/components/Layout/LayoutContent.jsx
@@ -56,36 +56,41 @@ const countryData = [
 		visaInfoEn: 'Visa-free regime for up to 90 days from 15.03.2020.',
 		legalBasisUz: "O'zbekiston va Turkiya o'rtasidagi ikki tomonlama kelishuv.",
 		legalBasisRu: 'Двустороннее соглашение между Узбекистаном и Турцией.',
-		legalBasisEn: 'Bilateral agreement between Uzbekistan and Türkiye.',
+		legalBasisEn:
+			'Starting from February 1, 2019, entry is allowed for up to 30 days with an electronic visa (e-visa.gov.uz)',
 	},
 	{
 		code: 'ALB',
 		nameUz: 'Albaniya Respublikasi',
 		nameRu: 'Республика Албания',
-		nameEn: 'Republic of Türkiye',
+		nameEn: 'Republic of Albania',
 		visaInfoUz:
 			'01.02.2019 yildan 30 kungacha elektron kirish vizasi bilan (e-viza.gov.uz).',
 		visaInfoRu:
 			'с 01.02.2019 года до 30 суток с электронной въездной визой (e-viza.gov.uz). ',
-		visaInfoEn: 'Visa-free regime for up to 90 days from 15.03.2020.',
+		visaInfoEn:
+			'According to the Presidential Decree of the Republic of Uzbekistan No. PF-5611 dated January 5, 2019',
 		legalBasisUz: "O'zbekiston va Turkiya o'rtasidagi ikki tomonlama kelishuv.",
 		legalBasisRu: 'Двустороннее соглашение между Узбекистаном и Турцией.',
 		legalBasisEn: 'Bilateral agreement between Uzbekistan and Türkiye.',
 	},
 	{
 		code: 'AND',
-		nameUz: 'Turkiya Respublikasi',
-		nameRu: 'Турецкая Республика',
+		nameUz: 'Andorra knyazligi',
+		nameRu: 'Andorra',
 		nameEn: 'Republic of Türkiye',
 		visaInfoUz:
 			'01.02.2019 yildan boshlab 30 kungacha bir tomonlama vizasiz rejim.',
-		visaInfoRu: 'с 15.03.2020 года безвизовый режим до 90 суток.',
-		visaInfoEn: 'Visa-free regime for up to 90 days from 15.03.2020.',
+		visaInfoRu:
+			'с 01.02. 2019 года односторонний безвизовый режим до 30 суток.',
+		visaInfoEn:
+			'Starting from February 1, 2019, a unilateral visa-free regime for up to 30 days is established.',
 		legalBasisUz:
 			'O’zbekiston Respublikasi Prezidentining 05.01.2019 yildagi PF-5611 sonli farmoni va O‘zbekiston Respublikasi Vazirlar Maxkamasining 21.11.1996 yildagi 408-sonli Qarori.',
 		legalBasisRu: `Постановление Президента Республики Узбекистан 
 № ПП-5611 от 05.01.2019 года и Постановление Кабинета Министров Республики Узбекистана № 408 от 21.11.1996 года.`,
-		legalBasisEn: 'Bilateral agreement between Uzbekistan and Türkiye.',
+		legalBasisEn:
+			'According to the Presidential Decree of the Republic of Uzbekistan No. PF-5611 dated January 5, 2019, and the Resolution of the Cabinet of Ministers of the Republic of Uzbekistan No. 408 dated November 21, 1996',
 	},
 ]
 
